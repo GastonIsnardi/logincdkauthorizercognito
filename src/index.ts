11e11556a175b824006293dc1b33cab31b1a6236
gaston isnardi <gaston.isnardi@cloudhesive.com>
2024-08-22 16:48:18 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// CDK
+// TODO crear lambda del crear usuario
+// TODO crear lambda del login
+// TODO crear lambda del logout
+// TODO crear lambda del obtener usuario
+// TODO crear lambda del actualizar usuario
+// TODO crear stack de lambdas
+// TODO crear stack de apiGateway
+// TODO crear stack de cognito
+// TODO crear authorizer de cognito con el apigateway
