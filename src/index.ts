0816412c0e8121a60e83efaf362f00f4e761331f
gaston isnardi <gaston.isnardi@cloudhesive.com>
2024-08-22 16:51:26 -0300

    todos

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 // CDK
-// TODO crear lambda del crear usuario
-// TODO crear lambda del login
-// TODO crear lambda del logout
-// TODO crear lambda del obtener usuario
-// TODO crear lambda del actualizar usuario
-// TODO crear stack de lambdas
-// TODO crear stack de apiGateway
-// TODO crear stack de cognito
-// TODO crear authorizer de cognito con el apigateway
+// TODO #1 crear stack de lambdas
+// TODO #2 crear stack de apiGateway
+// TODO #3 crear stack de cognito
+// TODO #4 crear authorizer de cognito con el apigateway
+// TODO #5 crear lambda del crear usuario
+// TODO #6 crear lambda del login
+// TODO #7 crear lambda del logout
+// TODO #8 crear lambda del obtener usuario
+// TODO #9 crear lambda del actualizar usuario
